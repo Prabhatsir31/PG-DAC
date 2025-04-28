e1dbcff29b6121e8fed92592e1f99469fc89f70c
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-04-28 17:01:14 +0000

    .NET-commit

---
+++ Microsoft_NET_Technologies/Class_Notes/C# Day-13/nextmiddlewaredemo/nextmiddlewaredemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nextmiddlewaredemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Microsoft_NET_Technologies/Class_Notes/C# Day-13/nextmiddlewaredemo/nextmiddlewaredemo/Models/custommiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace nextmiddlewaredemo.Models
+{
+    public class Custommiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public Custommiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync("Hello mmmmm Custom Middleware \n");
+            await _next(httpContext);
+            await httpContext.Response.WriteAsync("bye---- Middleware \n");
+
+        }
+
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Custommiddleware>();
+        }
+    }
+
+}
