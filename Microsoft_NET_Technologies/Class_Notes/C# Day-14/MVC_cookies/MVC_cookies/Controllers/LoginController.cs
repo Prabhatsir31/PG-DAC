18126f0be766ebd44483bb51ad29e67019634bbd
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-04-28 17:16:44 +0000

    .NET-commit

---
+++ Microsoft_NET_Technologies/Class_Notes/C# Day-14/MVC_cookies/MVC_cookies/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_cookies.Models;
+
+public class LoginController : Controller
+{
+    public IActionResult Loginform()
+    {
+        var user = new User();
+
+        if (Request.Cookies["LastEmail"] != null)
+        {
+            user.Email = Request.Cookies["LastEmail"];
+        }
+
+        return View(user);
+    }
+
+    [HttpPost]
+    public IActionResult Loginform(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            if (user.Remember)
+            {
+                CookieOptions option = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(7)
+                };
+                Response.Cookies.Append("LastEmail", user.Email, option);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(user);
+    }
+}
---
+++ Microsoft_NET_Technologies/Class_Notes/C# Day-14/MVC_cookies/MVC_cookies/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace MVC_cookies.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Remember { get; set; }
+    }
+
+}
