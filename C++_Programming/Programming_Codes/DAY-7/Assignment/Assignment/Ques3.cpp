c1c92f043f803e1db3168140f41626e78bc921a7
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-05-29 12:36:16 +0000

    C++-commit

--- C++_Programming/Programming_Codes/DAY-7/Assignment/Assignment/Ques3.cpp
+++ C++_Programming/Programming_Codes/DAY-7/Assignment/Assignment/Ques3.cpp
@@ -1,7 +1,70 @@
-//3)Create a base class Cricket.Define virtual function “void play()” in it.
+//3)Create a base class Cricket.Define virtual function ï¿½void play()ï¿½ in it.
 //Define following sub classes for this class.A) FiftyOver  b) Test  c) TwentyOver
-//“Test” class will have one more function “daywise_summary()”.create a global function “void doit() which accepts 
-//reference of type Cricket so that it can invoke “play()” function polymorphically.Inside this function find out 
-//where exactly where “Test” is stored using  RTTI(dynamic_cast), and invoke “daywise_summary()” 
-//function along with “play()” function.[handle bad_cast exception]
+//ï¿½Testï¿½ class will have one more function ï¿½daywise_summary()ï¿½.create a global function ï¿½void doit() which accepts 
+//reference of type Cricket so that it can invoke ï¿½play()ï¿½ function polymorphically.Inside this function find out 
+//where exactly where ï¿½Testï¿½ is stored using  RTTI(dynamic_cast), and invoke ï¿½daywise_summary()ï¿½ 
+//function along with ï¿½play()ï¿½ function.[handle bad_cast exception]
 
+
+//#include <iostream>
+//using namespace std;
+//
+//class Cricket {
+//public:
+//	virtual void play() {
+//		cout << "Playing cricket" << endl;
+//	}
+//	virtual ~Cricket() {}
+//};
+//
+//class FiftyOver : public Cricket {
+//public:
+//	void play() override {
+//		cout << "Playing Fifty Over Cricket" << endl;
+//	}
+//};
+//
+//class Test : public Cricket {
+//public:
+//	void play() override {
+//		cout << "Playing Test Cricket" << endl;
+//	}
+//	void daywise_summary() {
+//		cout << "Test Cricket Daywise Summary" << endl;
+//	}
+//};
+//
+//class TwentyOver : public Cricket {
+//public:
+//	void play() override {
+//		cout << "Playing Twenty Over Cricket" << endl;
+//	}
+//};
+//
+//void doit(Cricket& c) {
+//	try {
+//		c.play();
+//		Test* t = dynamic_cast<Test*>(&c);
+//		if (t) {
+//			t->daywise_summary();
+//		}
+//		else {
+//			throw bad_cast();
+//		}
+//	}
+//	catch (const bad_cast& e) {
+//		cout << "Caught bad_cast exception: " << e.what() << endl;
+//	}
+//}
+//
+//int main() {
+//	Cricket* matches[3];
+//	matches[0] = new FiftyOver();
+//	matches[1] = new Test();
+//	matches[2] = new TwentyOver();
+//	for (int i = 0; i < 3; i++) {
+//		doit(*matches[i]);
+//		delete matches[i];
+//	}
+//	return 0;
+//}
